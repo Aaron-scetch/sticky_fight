7f8090cdfff1ea9f20c4e0994d8ed77c633bc4b3
Aaron-scetch <scholzaaron99@gmail.com>
2025-12-27 13:49:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server läuft auf Port ${port}`);
+});
 
 // Erlaubt Anfragen von Ihrer HTML-Seite
 app.use(cors({
-  origin: 'https://aaron.royalart.de' 
+  origin: 'https://aaron.royalart.de.de' 
 }));
 
 app.get('/api/data', (req, res) => {
